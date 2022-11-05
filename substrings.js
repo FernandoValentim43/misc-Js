ba743284b060f40c337f5a09864b7e765335b00b
fernando <fernandoreal.valentim@gmail.com>
2022-11-04 22:24:20 -0300

    .'

--- substrings.js
+++ substrings.js
@@ -4,11 +4,17 @@ let string =
 string = string.replaceAll(" ", ""); //formata a string
 
 let subString = ["a", "em", "i", "el"];
-let grid = [...Array(subString.length)].map((e) => Array(2));
+let  grid = [...Array(subString.length)].map(e => Array(2));
 
-for (let i = 0; i < subString.length; i++) {
-  grid[i][0] = string.split(subString[i]).length - 1;
-  grid[j][1] = subString[i];
+for (let j = 0; j < subString.length; j++) {
+  for (let i = 0; i < subString.length; i++) {
+    grid[i][0] = string.split(subString[i]).length - 1;
+     grid[j][1] = subString[i]
+  }
 }
+
+grid.sort(function(a,b) {
+    return b[0]-a[0] 
+});
 
 console.log(grid);
