9ec24af75f4974124f8c8dd63e039ab468588ccb
fernando <fernandoreal.valentim@gmail.com>
2022-11-04 21:02:12 -0300

    .

--- findNumber.js
+++ findNumber.js
@@ -1,9 +1,22 @@
 let number = 12345
 number =  number.toString()
+let secretNumber = 365
+secretNumber =  secretNumber.toString()
+let element = false
+for(let i = 0 ; i <= number.length ; i++) {
 
-for(let i = 1 ; i <= number.length ; i++) {
+    tempNumber = number.replace(i.toString(), '')
+    for(let j = 0 ; j <= number.length ; j++) {
+        tempNumber =  tempNumber.replace(j.toString(), '')
 
-    tempNumber = number.replace(i.toString(), '⭕')
-    console.log(tempNumber)
-   
+        if(tempNumber == secretNumber) {
+            element = true
+        } 
+
+    }
+
 }
+
+console.log(element)
+
+
