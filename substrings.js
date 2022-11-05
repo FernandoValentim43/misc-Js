f8e95f25f448266700197d50487fd53612637536
fernando <fernandoreal.valentim@gmail.com>
2022-11-04 22:21:09 -0300

    .'

--- substrings.js
+++ substrings.js
@@ -1,5 +1,14 @@
-let string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+let string =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
-string = string.replaceAll(' ', '')
+string = string.replaceAll(" ", ""); //formata a string
 
-let subString =  ["a", "em", "i", "el"]
+let subString = ["a", "em", "i", "el"];
+let grid = [...Array(subString.length)].map((e) => Array(2));
+
+for (let i = 0; i < subString.length; i++) {
+  grid[i][0] = string.split(subString[i]).length - 1;
+  grid[j][1] = subString[i];
+}
+
+console.log(grid);
