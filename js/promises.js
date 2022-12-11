22183c3c614e5f733b478886c76143d71aad265d
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-12-11 14:26:37 -0300

    .

--- js/promises.js
+++ js/promises.js
@@ -1,3 +1,15 @@
 const random = () => {
-    return Math.floor(Math.random() * 2 + 1);
-}
+  return (Math.floor(Math.random() * 2 + 1) + Math.random()) * 1000;
+};
+
+const wait = (msg) => {
+  setTimeout(() => {
+    console.log({ msg });
+  }, random());
+};
+
+
+wait(1);
+wait(2);
+wait(3);
+wait(4);
